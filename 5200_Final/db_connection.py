b5b0a277556eacc8cb8ddc8141ad3a3dafd4fe06
Siddhant Saha <siddhantsaha@Siddhants-MacBook-Air.local>
2024-12-08 18:52:45 -0500

    PWW Database: Added drop roles + made password string empty for local development

--- 5200_Final/db_connection.py
+++ 5200_Final/db_connection.py
@@ -2,13 +2,14 @@ import streamlit as st
 import mysql.connector
 from mysql.connector import Error
 
+# connect to the database, input your local password for 'root' user
 def init_connection():
     """Initialize database connection using mysql-connector-python"""
     try:
         connection = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="dev",
+            password="",
             database="PWW_DATABASE",
             unix_socket="/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock"
         )
